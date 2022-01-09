6c884d3d8128212b98c16f46b5c51916fffaca73
Yuehan Wang <yuehan@cs.uchicago.edu>
2022-01-09 13:15:14 -0500

    start btn text added

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ pygame.display.set_caption('Our Game')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('assets/font/Pixeltype.ttf',50)
 
-def create_button(x, y, width, height, hovercolor, defaultcolor):
+#sounds
+start_sound = pygame.mixer.Sound('assets/sound/start.wav')
+#--------
+
+def create_button(x, y, width, height, hovercolor, defaultcolor, message):
     mouse = pygame.mouse.get_pos()
     # Mouse get pressed can run without an integer, but needs a 3 or 5 to indicate how many buttons
     click = pygame.mouse.get_pressed(3)
+    btn_text_surf = test_font.render(message, True,hovercolor)
+    btn_text_surf_hover = test_font.render(message, True,defaultcolor)
+    btn_text_rect = btn_text_surf.get_rect(midtop = (x + 60, y))
     if x + width > mouse[0] > x and y + height > mouse[1] > y:
         pygame.draw.rect(WIN, hovercolor, (x, y, width, height))
+        WIN.blit(btn_text_surf_hover,btn_text_rect)
         if click[0] == 1:
+            pygame.mixer.Sound.play(start_sound)
             return True
     else:
         pygame.draw.rect(WIN, defaultcolor, (x, y, width, height))
+        WIN.blit(btn_text_surf,btn_text_rect)
 
 def game_intro():
     #welcome page text
@@ -30,7 +40,7 @@ def game_intro():
         WIN.fill((139,0,18))
         WIN.blit(welcome_text_surf,welcome_rect)
         WIN.blit(start_text_surf,start_rect)
-        start_button = create_button(500, 300, 125, 26, 'Gold', 'White')
+        start_button = create_button(500, 300, 125, 26, (139,0,18), 'White','Start')
         if start_button:
             game()
         for event in pygame.event.get():
