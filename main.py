68fa2847d02a086813d02e33c62de434805ed875
Jaylan Zhao <natarichjay@gmail.com>
2022-01-08 23:29:04 -0500

    Set up initial pygame window

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from index import Index
+import pygame
+
+WIDTH, HEIGHT = 960, 600
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
 
 def main():
-    index_1 = Index("hello", 2)
-
-    print(index_1.id_str)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    
+    pygame.quit()
 
 
 if __name__ == "__main__":
